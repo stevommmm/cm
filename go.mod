19ff821332ebe17294ac42cae435425bd89420e6
smcgregor <s.mcgregor@griffith.edu.au>
2025-02-03 16:14:31 +1000

    ditch mpris package, listen for add dbus music related events and render

--- go.mod
+++ go.mod
@@ -3,12 +3,9 @@ module github.com/stevommmm/cm
 go 1.23.4
 
 require (
-	github.com/leberKleber/go-mpris v0.3.0
+	github.com/godbus/dbus/v5 v5.1.0
 	golang.org/x/image v0.23.0
 	golang.org/x/term v0.28.0
 )
 
-require (
-	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-)
+require golang.org/x/sys v0.29.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"image"
-	"image/png"
 	"log"
-	"net/url"
+	"fmt"
 	"os"
+	"image/png"
+	"image"
+	"net/url"
 	"syscall"
-	"time"
 
-	"github.com/leberKleber/go-mpris"
 	"golang.org/x/image/draw"
 	"golang.org/x/term"
+
+	"github.com/godbus/dbus/v5"
 )
 
 const (
@@ -25,20 +24,44 @@ const (
 )
 
 var (
-	ANSI_STATUS_SYM = map[mpris.PlaybackStatus]string{
-		mpris.PlaybackStatusPlaying: "⏵",
-		mpris.PlaybackStatusPaused:  "⏸",
-		mpris.PlaybackStatusStopped: "⏹",
+	ANSI_STATUS_SYM = map[string]string{
+		"Playing": "⏵",
+		"Paused":  "⏸",
+		"Stopped": "⏹",
 	}
-	mpris_player  string
 	album_art_max int = 20
+
+	// global state used for rendering
+	art    string
+	title  string
+	artist string
+	album  string
+	state  string = "Playing"
 )
 
-func renderLoop(p *mpris.Player) {
-	m, err := p.Metadata()
-	if err != nil {
-		return
+func get(m map[string]dbus.Variant, key string) string {
+	if newtitle, ok := m[key]; ok {
+		v := newtitle.Value()
+		if v == nil {
+			return ""
+		}
+		return v.(string)
 	}
+	return ""
+}
+
+func getone(m map[string]dbus.Variant, key string) string {
+	if newtitle, ok := m[key]; ok {
+		v := newtitle.Value()
+		if v == nil {
+			return ""
+		}
+		return v.([]string)[0]
+	}
+	return ""
+}
+
+func render() {
 	_, h, err := term.GetSize(syscall.Stdout)
 	if err == nil {
 		album_art_max = h * 2
@@ -47,16 +70,14 @@ func renderLoop(p *mpris.Player) {
 	dst := image.NewRGBA(image.Rect(0, 0, album_art_max, album_art_max))
 
 	// Try and parse supplied media url, otherwise use our blank image
-	if art, err := m.MPRISArtURL(); err == nil {
-		if u, err := url.Parse(art); err == nil {
-			f, err := os.Open(u.Path)
+	if u, err := url.Parse(art); err == nil {
+		f, err := os.Open(u.Path)
+		if err == nil {
+			img, err := png.Decode(f)
 			if err == nil {
-				img, err := png.Decode(f)
-				if err == nil {
-					draw.NearestNeighbor.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
-				}
-				f.Close()
+				draw.NearestNeighbor.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
 			}
+			f.Close()
 		}
 	}
 	fmt.Print(ANSI_CLEAR, ANSI_HOME)
@@ -75,39 +96,78 @@ func renderLoop(p *mpris.Player) {
 		// Playing media information & newlines except for last
 		switch y {
 		case 0:
-			title, _ := m.XESAMTitle()
 			fmt.Printf(" \x1b[1m%s\x1b[0m\n", title)
 		case 2:
-			artists, _ := m.XESAMArtist()
-			fmt.Printf(" %s\n", artists[0])
+			fmt.Printf(" %s\n", artist)
 		case 4:
-			album, _ := m.XESAMAlbum()
 			fmt.Printf(" %s\n", album)
 		case 6:
-			status, err := p.PlaybackStatus()
-			if err == nil {
-				fmt.Printf(" \x1b[1m%s\x1b[0m\n", ANSI_STATUS_SYM[status])
-			}
+			fmt.Printf(" \x1b[1m%s\x1b[0m\n", ANSI_STATUS_SYM[state])
 		case album_art_max - 2:
 			// no newline
 		default:
 			fmt.Printf("\n")
 		}
 	}
+
 }
 
 func main() {
-	flag.StringVar(&mpris_player, "player", "org.mpris.MediaPlayer2.firefox.instance_1_32", "DBUS player name")
-	flag.IntVar(&album_art_max, "art", 20, "Album art pixel (re)size")
-	flag.Parse()
-	p, err := mpris.NewPlayer(mpris_player)
+	ch := make(chan *dbus.Signal, 100)
+	sigconn, err := dbus.ConnectSessionBus()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer p.Close()
+
+	sigconn.AddMatchSignal(
+		dbus.WithMatchObjectPath("/org/mpris/MediaPlayer2"),
+		dbus.WithMatchInterface("org.freedesktop.DBus.Properties"),
+		dbus.WithMatchMember("PropertiesChanged"),
+	)
+	sigconn.Signal(ch)
 
 	for {
-		renderLoop(&p)
-		time.Sleep(time.Second * 10)
+		signal, ok := <-ch
+		if !ok {
+			return
+		}
+		switch signal.Name {
+		case "org.freedesktop.DBus.Properties.PropertiesChanged":
+			interface_name := signal.Body[0].(string)
+			changed_properties := signal.Body[1].(map[string]dbus.Variant)
+			_ = signal.Body[2].([]string) // we dont use invalidated_properties
+
+			if interface_name != "org.mpris.MediaPlayer2.Player" {
+				log.Fatalf("Captured bad signals, has the DBUS api changed?\n%v\n", signal)
+			}
+
+			if newstate, ok := changed_properties["PlaybackStatus"]; ok {
+				state = newstate.Value().(string)
+			}
+
+			if metadata, ok := changed_properties["Metadata"]; ok {
+				songmeta := metadata.Value().(map[string]dbus.Variant)
+
+				art = get(songmeta, "mpris:artUrl")
+				album = get(songmeta, "xesam:album")
+				artist = getone(songmeta, "xesam:artist")
+				title = get(songmeta, "xesam:title")
+			}
+
+			render()
+		}
 	}
 }
+
+/*
+map[string]dbus.Variant{
+"mpris:artUrl":dbus.Variant{sig:dbus.Signature{str:"s"}, value:"file:///home/smcgregor/.mozilla/firefox/firefox-mpris/4531_241.png"},
+"mpris:length":dbus.Variant{sig:dbus.Signature{str:"x"}, value:217000000},
+"mpris:trackid":dbus.Variant{sig:dbus.Signature{str:"o"}, value:"/org/mpris/MediaPlayer2/firefox"},
+"xesam:album":dbus.Variant{sig:dbus.Signature{str:"s"}, value:"You're Welcome"},
+"xesam:artist":dbus.Variant{sig:dbus.Signature{str:"as"}, value:[]string{"A Day To Remember"}},
+"xesam:title":dbus.Variant{sig:dbus.Signature{str:"s"}, value:"Viva La Mexico"},
+"xesam:url":dbus.Variant{sig:dbus.Signature{str:"s"}, value:"https://music.youtube.com/watch?v=9OK6_7qLhzY&list=MLCT"}
+}
+
+*/
